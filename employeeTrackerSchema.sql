6d9ec0c2581a6b19c72a24c36b39894ac10ef2ea
Edita Pesa <editapesa@gmail.com>
2021-04-21 17:15:54 -0400

    create employee tracker schema

---
+++ employeeTrackerSchema.sql
@@ -0,0 +1,27 @@
+drop database if exists employeeTracker_db;
+create database employeeTracker_db;
+
+use employeeTracker_db;
+
+create table department (
+    id integer not null auto_increment,
+    name varchar(30) not null,
+    primary key (id)
+);
+
+create table role (
+    id integer not null auto_increment,
+    title varchar(30) not null,
+    salary decimal(P, 2) not null,
+    department_id integer not null auto_increment,
+    primary key (id)
+);
+
+create table employee (
+    id integer not null auto_increment,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    role_id integer not null auto_increment,
+    manager_id integer null auto_increment,
+    primary key (id)
+);
