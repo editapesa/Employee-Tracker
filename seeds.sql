2f0bb3873080bc4109b475f1b520f66aa376343d
Edita Pesa <editapesa@gmail.com>
2021-04-21 18:55:08 -0400

    create seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+use employeeTracker_db;
+
+insert into department (name)
+values ('Sales'), ('Engineering'), ('Legal'), ('Finance');
+
+insert into role (title)
+values ('Sales Lead'), ('Salesperson'), ('Lead Engineer'), ('Software Engineer'), ('Lawyer'), ('Legal Assistant'), ('Accountant');
+
+insert into employee (first_name, last_name) values ('Gia', 'Sanchez');
+insert into employee (first_name, last_name) values ('Leo', 'Armon');
+insert into employee (first_name, last_name) values ('Nicky', 'Jams');
+insert into employee (first_name, last_name) values ('Chris', 'Cornell');
+insert into employee (first_name, last_name) values ('Jenny', 'Anderson');
+insert into employee (first_name, last_name) values ('Lola', 'Mess');
+insert into employee (first_name, last_name) values ('Anthony', 'Gallo');
+insert into employee (first_name, last_name) values ('Frankie', 'Furter');
