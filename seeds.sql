606f077203ad406788480980d8d4a9ca910d19b8
Edita Pesa <editapesa@gmail.com>
2021-04-22 14:50:03 -0400

    add inner joins in seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -14,3 +14,19 @@ insert into employee (first_name, last_name) values ('Jenny', 'Anderson');
 insert into employee (first_name, last_name) values ('Lola', 'Mess');
 insert into employee (first_name, last_name) values ('Anthony', 'Gallo');
 insert into employee (first_name, last_name) values ('Frankie', 'Furter');
+
+select * from department;
+select * from role;
+select * from employee;
+
+select title, first_name, last_name 
+from role
+inner join employee on role.title = employee.title;
+
+select title, name
+from department
+inner join role on department.name = title.department;
+
+select name, first_name, last_name
+from department
+inner join employee on department.name = employee.name; 
