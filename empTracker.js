a03af13e91b15035184b7f174a4baaadf57dbea3
Edita Pesa <editapesa@gmail.com>
2021-04-22 16:04:17 -0400

    add inquirer .prompt questions & const to start app

--- empTracker.js
+++ empTracker.js
@@ -9,3 +9,37 @@ const connection = mysql.createConnection({
     password: 'brutus',
     database: 'employeeTracker_db',
 });
+
+connection.connect((err) => {
+    if (err) throw err;
+    start();
+})
+
+const start = () => {
+    inquirer
+        .prompt({
+            name: 'whatToDo',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['View departments', 'View roles', 'View employees', 'Add a department', 'Add a role', 'Add an employee', 'Update employee roles', 'Exit'],
+        })
+        .then((answer) => {
+            if (answer.whatToDo === 'View departments') {
+                viewDept();
+            } else if (answer.whatToDo === 'View roles') {
+                viewRoles();
+            } else if (answer.whatToDo === 'View employees') {
+                viewEmp();
+            } else if (answer.whatToDo === 'Add a department') {
+                addDept();
+            } else if (answer.whatToDo === 'Add a role') {
+                addRole();
+            } else if (answer.whatToDo === 'Add an employee') {
+                addEmp();
+            } else if (answer.whatToDo === 'Update employee roles') {
+                updateEmpRole();
+            } else {
+                connection.end();
+            }
+        });
+};
